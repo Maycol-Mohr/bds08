021853388d018f0dcb25ac4ea2a051d2b37dcc72
Maycol Mohr <maycolmohr@yahoo.com.br>
2022-02-12 15:05:32 +0100

    aulas 12-16 / 12-17 / 12-18 da tarefa final

--- front-web/src/components/sales-by-store/index.tsx
+++ front-web/src/components/sales-by-store/index.tsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
+import { makeRequest } from '../../utils/request';
 import PieChartCard from '../pie-chart-card';
 import './styles.css';
+import { ChartSeriesData, SalesByGender } from '../../types';
+import { buildChartSeries, sumSalesByGender } from './helpers';
+import { formatPrice } from '../../utils/formatters';
 
 function SalesByStore() {
+  const [chartSeries, setChartSeries] = useState<ChartSeriesData[]>([]);
+  const [totalSum, setTotalSum] = useState(0);
+
+  useEffect(() => {
+    makeRequest.get<SalesByGender[]>('/sales/by-gender?storeId=0').then((response) => {
+      const newChartSeries = buildChartSeries(response.data);
+      setChartSeries(newChartSeries);
+      const newTotalSum = sumSalesByGender(response.data);
+      setTotalSum(newTotalSum);
+    });
+  }, []);
+
   return (
     <div className="sales-by-store-container base-card">
       <div className="sales-by-store-data">
-        <h1>R$ 746.484,00</h1>
+        <h1>{formatPrice(totalSum)}</h1>
         <span>Total de vendas</span>
       </div>
       <div className="pie-chart-card">
---
+++ front-web/src/types.ts
@@ -0,0 +1,9 @@
+export type SalesByGender = {
+  gender: string;
+  sum: number;
+};
+
+export type ChartSeriesData = {
+  x: string;
+  y: number;
+};
